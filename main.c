35bf66fbbdc6121735e3b0c3a0a62fcf6e2b0ed5
Leah <shlemov@tuta.io>
2025-01-02 19:38:46 +0200

    Added todo

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include "cards.h"
 
+/**
+ * Blackjack gamestate:
+ * - Deck
+ * - Dealer's hand
+ * - Player's hand
+ * - Cash = 1000
+ * - Pot = 0
+ * - Dealer's hand value
+ * - Player's hand value
+ */
+
 int main(void) {
+    // 1. INITIALIZATION
+    // init full deck
+    // init empty dealer's hand
+    // init empty player's hand
+    // 2. BETTING
+    // check for empty cash
+    // print game over if no cash
+    // print message asking the player to bet
+    // 3. INITIAL DEAL
+    // draw 2 cards for each player
+    // 4. BLACKJACK CHECK
+    // if blackjack end game
+    // 5. HIT OR STAND
+    // 6. DEALR"S DRAW
+    // 7. RESET
     return 0;
 }
