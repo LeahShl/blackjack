cd53f450d60aa531913e30a935149b2ce3fb9e2e
Leah <shlemov@tuta.io>
2025-01-09 14:34:14 +0200

    cards.c mostly done and working

--- cards.c
+++ cards.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 // RANK CONSTS
 const uint8_t R_MASK = 0xF0;
@@ -24,6 +26,9 @@ const uint8_t CLUBS = 0x02;
 const uint8_t DIAMOND = 0x04;
 const uint8_t SPADES = 0x08;
 
+// OTHER CONSTS
+const size_t CARD_STR_SIZE = 6;
+
 // STRUCT DEFINES
 typedef struct Card_t Card_t;
 struct Card_t
@@ -44,13 +49,36 @@ struct Deck_t
 //////////////////////////////////
 
 /**
+ * Creates a new card.
+ * Doesn't check data for correctness.
+ */
+Card_t * new_card(uint8_t rank, uint8_t suit){
+    Card_t *c = malloc(sizeof(Card_t));
+    //TODO: check if failed
+    c->data = rank | suit;
+    c->next = NULL;
+    return c;
+}
+
+/**
  * Initializes an empty deck
  */
 Deck_t * init_empty_deck(){
-    Deck_t *d = {0};
+    Deck_t *d = malloc(sizeof(Deck_t));
+    //TODO: check if failed
     d->head = NULL;
     d->len = 0;
     return d;
+}
+
+/**
+ * Adds card c to the top (head) of deck d.
+ * Will override whatever next card c is pointing to.
+ */
+void addh_card(Deck_t *d, Card_t *c){
+    c->next = d->head;
+    d->head = c;
+    d->len += 1;
 }
 
 /**
@@ -60,7 +88,8 @@ Deck_t * init_full_deck(){
     Deck_t *d = init_empty_deck();
     for(uint8_t suit = 0x01; suit <= 0x08; suit *= 0x02){
         for(uint8_t rank = 0x10; rank <= 0xD0; rank += 0x10){
-            // TODO: finish after addh_card
+            Card_t *c = new_card(rank, suit);
+            addh_card(d, c);
         }
     }
     return d;
@@ -68,21 +97,40 @@ Deck_t * init_full_deck(){
 
 /**
  * Selects the nth card from deck d.
+ * The head is the 0th card.
  */
-Card_t * draw_at(Deck_t *d, int n);
+Card_t * draw_at(Deck_t *d, size_t n){
+    Card_t *curr = d->head, *prev = NULL;
+    for (size_t i = 0; curr != NULL && i < n; i++)
+    {
+        prev = curr;
+        curr = curr->next;
+    }
+    
+    // n bigger than size of deck
+    if(curr == NULL){
+        return NULL;
+    }
+
+    if(prev == NULL){
+        d->head = curr->next; 
+    }
+    else{
+        prev->next = curr->next; 
+    }
+
+    curr->next = NULL;
+    d->len -= 1;
+    return curr;
+}
 
 /**
  * Selects the card at the top of deck d.
  */
-Card_t * draw_top(Deck_t *d);
-
-/**
- * Adds card c to the top (head) of deck d.
- * Will override whatever next card c is pointing to.
- */
-void addh_card(Deck_t *d, Card_t *c){
-    c->next = d->head;
-    d->head = c;
+Card_t * draw_top(Deck_t *d){
+    Card_t *c = d->head;
+    d->head = d->head->next;
+    return c;
 }
 
 /**
@@ -90,17 +138,15 @@ void addh_card(Deck_t *d, Card_t *c){
  */
 void addt_card(Deck_t *d, Card_t *c){
     Card_t *p = d->head;
-    for (size_t i = 0; i < d->len; i++)
+    while(p->next)
     {
         p = p->next;
     }
-    
+    p->next = c;
+    c->next = NULL;
+    d->len += 1;
 }
 
-/**
- * Copies a string representation of card c into string s.
- * dest needs to point to a char[] with a size of at least 6.
- */
 /**
  * Creates a null-terminated string representing card c
  */
@@ -163,9 +209,26 @@ void card_str(Card_t *c, char *s){
 /**
  * Prints an entire deck to stdout
  */
-void print_deck(Deck_t *d);
+void print_deck(Deck_t *d){
+    Card_t *c = d->head;
+    while(c){
+        char s[CARD_STR_SIZE];
+        card_str(c, s);
+        printf("[ %s ] ", s);
+        c = c->next;
+    }
+    puts("\n");
+}
 
 /**
  * Frees the allocated memory for deck d.
  */
-void free_deck(Deck_t *d);
+void free_deck(Deck_t *d){
+    Card_t *c = d->head;
+    while(c){
+        d->head = d->head->next;
+        free(c);
+        c = d->head;
+    }
+    free(d);
+}
